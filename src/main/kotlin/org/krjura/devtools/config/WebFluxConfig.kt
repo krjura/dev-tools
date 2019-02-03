69b157381782e5c87b18f03b5037012a432bdb68
Krešimir Jurasović <kjurasovic@outlook.com>
2019-02-03 16:14:47 +0100

    enabled static resource compression and caching

--- src/main/kotlin/org/krjura/devtools/config/WebFluxConfig.kt
+++ src/main/kotlin/org/krjura/devtools/config/WebFluxConfig.kt
@@ -4,11 +4,23 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class WebFluxConfig : WebFluxConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+
+        val cacheControl = CacheControl
+            .maxAge(7, TimeUnit.DAYS)
+            .noTransform()
+            .cachePrivate();
+
+        registry
+            .addResourceHandler("/portal/*.js", "/portal/*.css", "/portal/*.woff", "/portal/*.woff2")
+            .addResourceLocations("file:web-resources/")
+            .setCacheControl(cacheControl);
+
         registry
             .addResourceHandler("/portal/**")
             .addResourceLocations("file:web-resources/")
