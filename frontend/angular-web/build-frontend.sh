efa07433bc9e06d35058cedf4bab6c7918d4eb71
Krešimir Jurasović <kjurasovic@outlook.com>
2021-02-06 20:24:59 +0100

    feat: upgraded node version to 14.15.4

--- frontend/angular-web/build-frontend.sh
+++ frontend/angular-web/build-frontend.sh
@@ -6,10 +6,6 @@ HOME=/tmp/build-user
 mkdir -p $HOME
 echo "Users temporary home is $HOME"
 
-mkdir -p $HOME/.git
-git config --global user.name "node"
-git config --global user.email "node@example.com"
-
 # delete dist
 rm -rf dist
 
