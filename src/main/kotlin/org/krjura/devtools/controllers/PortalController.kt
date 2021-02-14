eca316dc49128348eab37de53762e2f47363f77f
Krešimir Jurasović <kjurasovic@outlook.com>
2021-02-14 15:44:57 +0100

    fix: removed reference to spring webflux in portal controller

--- src/main/kotlin/org/krjura/devtools/controllers/PortalController.kt
+++ src/main/kotlin/org/krjura/devtools/controllers/PortalController.kt
@@ -3,16 +3,16 @@ package org.krjura.devtools.controllers
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
-import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import java.nio.file.Paths
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 class PortalController {
 
     @GetMapping(value = ["/", "/portal", "/portal/op/**"])
-    fun rootRedirect(response: ServerHttpResponse): ResponseEntity<Resource> {
+    fun rootRedirect(response: HttpServletResponse): ResponseEntity<Resource> {
         return ResponseEntity.ok(FileSystemResource(Paths.get("web-resources/index.html")))
     }
 }
